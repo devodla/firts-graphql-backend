401ced7822938ace7de2220e810f22d5d568dc73
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-03-14 03:11:46 -0300

    feat(main): add feature moongose for graphql

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,12 @@
-const users = [
-  { id: 1, name: "Reynaldo", email: "reynaldo@mc1.com.br"},
-  { id: 2, name: "Only Reynaldo", email: "onlyreynaldo@hotmail.com"}
-];
+const User = require("./schemas/UserSchema");
 
 module.exports = {
   Query: {
-    users: () => users,
-    user: () => users[0]
+    users: () => User.find(),
+    user: (_, { id }) => User.findById(id)
   },
 
   Mutation: {
-    createUser: () => users[0]
+    createUser: (_, { name, email }) => User.create({ name, email }),
   }
-}
+};
